363e049cc9ded3a85f9be5239fb36ee8ead110ca
Md. Pieash Ahmed <pieash9@gmail.com>
2025-01-04 12:41:02 +0600

    first commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css'
+import EditSVG from '@/assets/edit.svg'
+import Menhera from '@/assets/menhera.png'
+import Image from 'next/image'
+
+export default function Home() {
+	return (
+		<div className="flex flex-col items-center justify-center text-red-500 text-3xl">
+			test
+		</div>
+	)
+}
